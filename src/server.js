b0d02b507a2a10ccc87f773bad1feb791b4f3bf4
zaldyz <zaldy222@gmail.com>
2022-08-28 23:41:08 +1000

    added root and players/search route

--- src/server.js
+++ src/server.js
@@ -1,6 +1,19 @@
 import express, { json } from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
+import pkg from 'pg';
+const { Pool } = pkg;
+
+const pool = new Pool({database: 'myfut'});
+
+/*
+pool.query('SELECT * FROM users WHERE id = $1', [1], (err, res) => {
+  if (err) {
+    throw err
+  }
+  console.log('user:', res.rows[0])
+})
+*/
 
 const port = 3257;
 
@@ -14,11 +27,26 @@ const HOST = process.env.IP || 'localhost';
 
 // Root URL
 app.get('/', (req, res) => {
-  res.json(
-    {
-      message: "Welcome to the myFut web server!",
+  res.json({
+    message: "Welcome to the myFut web server!"
+  });
+});
+
+
+app.get('/players/search', (req, res, next) => {
+  pool.query(
+  `SELECT fullname, overall, position, club, league, pace, shooting, passing, dribbling, defence, physical 
+  FROM cards 
+  WHERE fullname ~* $1`, [req.query.name], (err, result) => {
+    if (err) {
+      return next(err);
     }
-  );
+    res.json(
+      {
+        players: result.rows,
+      }
+    );
+  })
 });
 
 /**
